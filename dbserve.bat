5e51c4cd2b2e683083bd613b7576bb78a3ab0d0d
QWEQW\Sameer Iqbal <sameer.iqbal@haball.pk>
2018-07-18 23:36:23 +0500

    Updated Version

--- dbserve.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:
-cd C:\Program Files\MongoDB\Server\4.0\bin
-mongod.exe --bind_ip 192.168.2.103 --dbpath D:\pulsatechs\test\data
-pause
--- routes/devices.js
+++ routes/devices.js
@@ -31,7 +31,7 @@ module.exports = function (app, db) {
             } else {
                 res.send({
                     error: false,
-                    message:  "Updated Successfully"
+                    message: "Updated Successfully"
                 })
             }
         });
@@ -46,7 +46,7 @@ module.exports = function (app, db) {
             } else {
                 res.send({
                     error: false,
-                    message:  "Added Successfully"
+                    message: "Added Successfully"
                 })
             }
         });
@@ -70,6 +70,77 @@ module.exports = function (app, db) {
             }
         });
     })
-
+    app.post('/devices/rename', (req, res) => {
+        var id = req.body._id;
+        var name = req.body.name;
+        var myquery = { _id: ObjectId(id) };
+        var newvalues = { $set: { name: name } };
+        db.collection("devices").updateOne(myquery, newvalues, function (err, item) {
+            if (err) {
+                res.send({
+                    error: true,
+                    message: err
+                });
+            } else {
+                res.send({
+                    error: false,
+                    message: "Updated Successfully"
+                })
+            }
+        });
+    })
+    app.post('/rooms/rename', (req, res) => {
+        var id = req.body._id;
+        var name = req.body.name;
+        var myquery = { _id: ObjectId(id) };
+        var newvalues = { $set: { name: name } };
+        db.collection("rooms").updateOne(myquery, newvalues, function (err, item) {
+            if (err) {
+                res.send({
+                    error: true,
+                    message: err
+                });
+            } else {
+                res.send({
+                    error: false,
+                    message: "Updated Successfully"
+                })
+            }
+        });
+    })
+    app.post('/rooms/devices', (req, res) => {
+        var pid = req.body.parent;
+        var query = { parent: ObjectId(pid) };
+        db.collection("devices").find(query).toArray(function (err, result) {
+            if (err) {
+                res.send({
+                    error: true,
+                    message: err
+                });
+            } else {
+                res.send({
+                    error: false,
+                    message: result
+                })
+            }
+        });
+    })
+    app.post('/rooms/sensors', (req, res) => {
+        var pid = req.body.parent;
+        var query = { parent: ObjectId(pid) };
+        db.collection("sensors").find(query).toArray(function (err, result) {
+            if (err) {
+                res.send({
+                    error: true,
+                    message: err
+                });
+            } else {
+                res.send({
+                    error: false,
+                    message: result
+                })
+            }
+        });
+    })
 
 }
